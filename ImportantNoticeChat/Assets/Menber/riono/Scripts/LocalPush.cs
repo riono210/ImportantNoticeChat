e04560740204603e38d9c5d9230cb66fb4840af5
Ryo Yasuda <e165729@ie.u-ryukyu.ac.jp>
2020-08-01 21:10:40 +0900

    プッシュ通知のテスト実装

---
+++ ImportantNoticeChat/Assets/Menber/riono/Scripts/LocalPush.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Notifications.iOS;
+using UnityEngine;
+
+public static class LocalPush {
+
+    // 通知の全削除
+    public static void AllClear () {
+        iOSNotificationCenter.RemoveAllScheduledNotifications ();
+        iOSNotificationCenter.RemoveAllDeliveredNotifications ();
+        // バッジの削除
+        iOSNotificationCenter.ApplicationBadge = 0;
+    }
+
+    // push通知の登録
+    public static void AddSchedule (string title, string message, int badgeCount, int elapsedTime) {
+        SetIOSNotification (title, message, badgeCount, elapsedTime);
+    }
+
+    private static void SetIOSNotification (string title, string message, int badgeCount, int elapsedTime) {
+        // 通知の作成
+        iOSNotificationCenter.ScheduleNotification (new iOSNotification {
+            Identifier = $"_notification_{badgeCount}",
+                Title = title,
+                Body = message,
+                ShowInForeground = false,
+                Badge = badgeCount,
+                Trigger = new iOSNotificationTimeIntervalTrigger () {
+                    TimeInterval = new TimeSpan (0, 0, elapsedTime),
+                        Repeats = false
+                }
+        });
+    }
+
+}
---
+++ ImportantNoticeChat/Assets/Menber/riono/Scripts/PushNotificationTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushNotificationTest : MonoBehaviour {
+    // // Start is called before the first frame update
+    // void Start () {
+    //     LocalPush.AddSchedule ("プッシュ通知のテスト", "korehatestdesuka?", 1, 10);
+    // }
+
+    // // Update is called once per frame
+    // void Update () {
+
+    // }
+
+    private void OnApplicationPause (bool pauseStatus) {
+        if (pauseStatus) {
+            LocalPush.AddSchedule ("プッシュ通知のテスト", "勝ったな...!", 1, 10);
+        }
+    }
+}
