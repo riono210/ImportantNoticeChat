30dc4bf0267d24d46f26fde70905dfbc7f9449e6
Ryo Yasuda <e165729@ie.u-ryukyu.ac.jp>
2020-08-04 16:58:25 +0900

    通知調整中

--- ImportantNoticeChat/Assets/Menber/riono/Scripts/PushNotificationTest.cs
+++ ImportantNoticeChat/Assets/Menber/riono/Scripts/PushNotificationTest.cs
@@ -1,21 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Unity.Notifications.iOS;
 using UnityEngine;
 
 public class PushNotificationTest : MonoBehaviour {
-    // // Start is called before the first frame update
-    // void Start () {
-    //     LocalPush.AddSchedule ("プッシュ通知のテスト", "korehatestdesuka?", 1, 10);
-    // }
+    int badgeCount = 1;
+    bool status;
 
-    // // Update is called once per frame
-    // void Update () {
-
-    // }
-
+    // バックグラウンド時の動作
     private void OnApplicationPause (bool pauseStatus) {
+        status = pauseStatus;
         if (pauseStatus) {
-            LocalPush.AddSchedule ("プッシュ通知のテスト", "勝ったな...!", 1, 10);
+            while (true) {
+                LocalPush.AddSchedule ("プッシュ通知のテスト", "勝ったな...!", badgeCount, 1);
+                badgeCount += 1;
+
+                Debug.Log ("pushhhhhh");
+                // LocalPush.AddSchedule ("プッシュ通知のテスト", "そのに", badgeCount, 1);
+                // badgeCount += 1;
+
+                if (!status) {
+                    Debug.Log ("Loop Finish!!!!");
+                }
+
+                WaitFunction (2f);
+
+                Debug.Log ("next loop");
+            }
+        }
+    }
+
+    // ゲームがフォーカスされているか
+    private void OnApplicationFocus (bool pauseStatus) {
+        status = !pauseStatus;
+        iOSNotificationCenter.ApplicationBadge = 0;
+    }
+
+    public IEnumerator ConsecutiveNotice () {
+
+        while (true) {
+            LocalPush.AddSchedule ("プッシュ通知のテスト", "勝ったな...!", badgeCount, 1);
+            badgeCount += 1;
+
+            Debug.Log ("pushhhhhh");
+            // LocalPush.AddSchedule ("プッシュ通知のテスト", "そのに", badgeCount, 1);
+            // badgeCount += 1;
+
+            if (!status) {
+                Debug.Log ("Loop Finish!!!!");
+                yield break;
+            }
+
+            yield return new WaitForSecondsRealtime (2f);
+
+            Debug.Log ("next loop");
+        }
+    }
+
+    public void WaitFunction (float time) {
+        float waitTime = Time.realtimeSinceStartup + time;
+        while (Time.realtimeSinceStartup < waitTime) {
+            Thread.Sleep (100);
         }
     }
 }
