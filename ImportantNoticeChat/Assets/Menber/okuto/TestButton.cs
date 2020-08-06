c8bf3744ff277ec2c0199546d62214505b8eed9b
Okuto Matsuda <e165758@ie.u-ryukyu.ac.jp>
2020-08-06 14:05:56 +0900

    テスト用のボタンを押すと、テスト用のメッセージを追加表示できるようにした。

---
+++ ImportantNoticeChat/Assets/Menber/okuto/TestButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButton : MonoBehaviour
+{
+    public GameObject myMessage;
+    public GameObject othersMessage;
+
+    public GameObject messages;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick(){
+        var message = new Dictionary<string, string>(){
+            {"content", "Hello !"},
+            {"name", "Aさん"}
+        };
+        this.appendMessage(message);
+    }
+
+    void appendMessage(Dictionary<string, string> message){	
+        GameObject mes = Instantiate(myMessage);
+        mes.transform.parent = messages.transform;
+    }
+}
