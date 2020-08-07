b572a7b101091393c11dd0bff4da37aa6db31f6b
Yuichi Kamezawa <e165721@ie.u-ryukyu.ac.jp>
2020-08-07 18:30:50 +0900

    Postソースコードのpushし直し

--- ImportantNoticeChat/Assets/Menber/kame/Scripts/ApiCall.cs
+++ ImportantNoticeChat/Assets/Menber/kame/Scripts/ApiCall.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class ApiCall : MonoBehaviour
-{
+public class ApiCall : MonoBehaviour {
     string url = "http://db.denchu.cloud:5111/uiuxchat3287bivsgfbivf/test2/messages";
     // アクセスキーは削除すること
     string key = "";
@@ -16,8 +20,9 @@ public class ApiCall : MonoBehaviour
         Debug.Log(url);
         Debug.Log(key);
         Debug.Log("message = " + message);
-        StartCoroutine(GetText());
-        // StartCoroutine(PostText());
+        // StartCoroutine(GetText());
+        StartCoroutine(PostText());
+        //PostText();
     }
 
     // Update is called once per frame
@@ -45,31 +50,37 @@ public class ApiCall : MonoBehaviour
         }
     }
 
-    /*
+    
     IEnumerator PostText() {
-        WWWForm data = new WWWForm();
-        data.AddField("to", "someone");
-        data.AddField("content", "hello");
+        // 投げるjsonデータ
+        string postData = "{\"to\":\"someone\",\"content\":\"iiiiiii\"}";
+        byte[] byteArray = Encoding.UTF8.GetBytes (postData);
 
-        // 参考になりそう: https://qiita.com/mosin_nozomi/items/1be9098b659be151b48a
-        var request = UnityWebRequest.Post(url, data);
-        request.SetRequestHeader("key", key);
-        yield return request.SendWebRequest();
-        
-        if(request.isHttpError) {
-            // レスポンスコードを見て処理
-            Debug.Log($"[Error]Response Code : {request.responseCode}");
-        }
-        else if (request.isNetworkError) {
-            // エラーメッセージを見て処理
-            Debug.Log($"[Error]Message : {request.error}");
-        }
-        else{
-            // 成功したときの処理
-            Debug.Log($"[Success]");
-            Debug.Log(request.downloadHandler.text);
+        // cookie書き込み
+        CookieContainer cc = new CookieContainer ();
+        cc.Add (new Uri (url), new Cookie ("key", key));
 
+        // リクエストの作成
+        HttpWebRequest req = (HttpWebRequest) WebRequest.Create (url);
+        req.Method = "POST";
+        req.ContentType = "application/json";
+        req.ContentLength = byteArray.Length;
+        req.CookieContainer = cc;
+
+        // ストリームに送信するデータを書き込む
+        Stream dataStream = req.GetRequestStream ();
+        dataStream.Write (byteArray, 0, byteArray.Length);
+        dataStream.Close ();
+
+        // 送信
+        HttpWebResponse res = (HttpWebResponse) req.GetResponse ();
+
+        for (int i = 0; i < res.Headers.Count; i++) {
+            Debug.Log (res.Headers[i]);
         }
+
+        yield return res;
+
     }
-    */
+    
 }
