f8e0999604d768b51557f57ee941288f62a3e165
Okuto Matsuda <e165758@ie.u-ryukyu.ac.jp>
2020-08-09 17:23:13 +0900

    (content, priority) を引数にPOSTできるようになった。

--- ImportantNoticeChat/Assets/Menber/kame/Scripts/ApiCall.cs
+++ ImportantNoticeChat/Assets/Menber/kame/Scripts/ApiCall.cs
@@ -9,21 +9,21 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class ApiCall : MonoBehaviour {
-    string url;
+    string baseURL;
     string key;
     string message = "(・∀・)ｲｲ!!";
 
     // Start is called before the first frame update
     void Start()
     {
-        url = Env.GetBaseUrl();
+        baseURL = Env.GetBaseUrl();
         key = Env.GetSecKey();
 
-        Debug.Log(url);
+        Debug.Log(baseURL);
         Debug.Log(key);
         Debug.Log("message = " + message);
-        StartCoroutine(GetText());
-        //StartCoroutine(PostText());
+        //StartCoroutine(GetText());
+//        StartCoroutine(PostText("messages", "text_test", 1));
         //PostText();
     }
 
@@ -33,7 +33,7 @@ public class ApiCall : MonoBehaviour {
     }
 
     IEnumerator GetText() {
-        var request = UnityWebRequest.Get(url+"messages");
+        var request = UnityWebRequest.Get(baseURL+"messages");
         yield return request.SendWebRequest();
 
         if(request.isHttpError) {
@@ -53,9 +53,13 @@ public class ApiCall : MonoBehaviour {
     }
 
     
-    IEnumerator PostText() {
+    public IEnumerator PostText(string endpoint, string content, int priority) {
+        string url = baseURL + endpoint;
         // 投げるjsonデータ
-        string postData = "{\"to\":\"someone\",\"content\":\"iiiiiii\"}";
+        string postData = "{\"to\":\"someone\", \"from\":\"someone\","
+        + "\"content\":\"" + content
+        + "\", \"priority\":\"" + priority.ToString() + "\"}";
+//        string postData = ($@"{"to:"someone", "from:"someone", "content":"{content}", "priority":{priority} }");
         byte[] byteArray = Encoding.UTF8.GetBytes (postData);
 
         // cookie書き込み
@@ -77,9 +81,9 @@ public class ApiCall : MonoBehaviour {
         // 送信
         HttpWebResponse res = (HttpWebResponse) req.GetResponse ();
 
-        for (int i = 0; i < res.Headers.Count; i++) {
-            Debug.Log (res.Headers[i]);
-        }
+//        for (int i = 0; i < res.Headers.Count; i++) {
+//            Debug.Log (res.Headers[i]);
+//        }
 
         yield return res;
 
